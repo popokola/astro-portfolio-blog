d35615e4916cb39fff42fff83ad72f2b3b32cd18
Charles Parames <55605195+popokola@users.noreply.github.com>
2024-05-06 18:21:37 +0200

    Update astro.config.mjs

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -4,12 +4,8 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://charlesparames.com',
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  output: "server",
-  adapter: vercel()
 });
